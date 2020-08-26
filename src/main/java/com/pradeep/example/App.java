a268c5e68df9901294c0400d87bfd8cb39d53430
pradeep shaga <pradeepreddyshaga@gmail.com>
2020-08-25 22:34:46 -0400

    Update Pom

---
+++ src/main/java/com/pradeep/example/App.java
@@ -0,0 +1,21 @@
+package com.pradeep.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@SpringBootApplication
+@PropertySources({})
+public class App implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class,args);
+	}
+
+	public void run (String...arg0) throws Exception {
+	
+	}
+}	
